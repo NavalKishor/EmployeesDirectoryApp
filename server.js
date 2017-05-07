a36ae490bf5dff3568a7a7e97edac2d261f32dcf
Naval Kishor Jha <er.navalkishor@yahoo.in>
2017-05-07 18:31:34 +0530

    favicon handled

--- server.js
+++ server.js
@@ -20,10 +20,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Routes
 app.use('/api', require('./routes/api'));
-
+app.get('/favicon.ico', function(req, res) {
+    res.send(204);
+});
 // Start server
-var port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-, ip = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
-app.listen(port, ip, function() {
-  console.log('Express server listening on %d', port);
+var port = process.env.PORT || 8080;
+// var port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+// , ip = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+//app.listen(port, ip, function() {
+var server=app.listen(port,  function(req,res) {
+  console.log('Express server listening  on http://localhost: %d', port);
 });
