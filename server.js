936a37e9af47b516f07657b0984e5bed67a21cda
Naval Kishor Jha <er.navalkishor@yahoo.in>
2017-05-07 19:53:06 +0530

    using express-favicon handle this

--- server.js
+++ server.js
@@ -6,38 +6,54 @@ var bodyParser = require('body-parser');
 
 // MongoDB
 
-mongoose.connect(process.env.OPENSHIFT_MONGODB_DB_URL || 'mongodb://user1:user1@ds133261.mlab.com:33261/employees-demo');
+mongoose.connect('mongodb://user1:user1@ds133261.mlab.com:33261/employees-demo');
 // mongoose.connect(process.env.OPENSHIFT_MONGODB_DB_URL || 'mongodb://localhost/employees-demo');
 // mongoose.connection.on('error', function(){});
 
+
+//2nd try favico
+const favicon = require('express-favicon');
+// var path = require('path')
+
+//end favicon
 // Express
 var app = express();
-
 app.use(express.static(__dirname + '/public'));
+app.use(favicon(path.join(__dirname+'public/images/favicon.ico')))
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 //to block the favicon req
-var http=require('http');
-var server=http.createServer(function(req,res){
-	if(req.method==='GET' && req.url==='/favicon.ico'){
-		var fs=require('fs');
-		fs.createReadStream('favicon.ico');;
-		fs.pipe(res); //this will replace the call to end
-	}
-	else
-	{
-		res.end();
-	}
-});
+// var http = require('http');
+
+// http.createServer(function (q, r) { 
+
+//   // control for favicon
+
+//   if (q.url === '/favicon.ico') {
+//     r.writeHead(200, {'Content-Type': 'image/x-icon'} );
+//     r.end();
+//     console.log('favicon requested');
+//     return;
+//   }
+
+//   // not the favicon? say hai
+//   console.log('hello');
+//   r.writeHead(200, {'Content-Type': 'text/plain'} );
+//   r.write('Hello, world!');
+//   r.end();
+  
+// }).listen(8000);
+
 //end favicon req
 
 
 // Routes
 app.use('/api', require('./routes/api'));
-app.get('/favicon.ico', function(req, res) {
-    res.send(200);
-});
+// app.get('/favicon.ico', function(req, res) {
+//     res.send(200);
+//     res.end();
+// });
 // Start server
 var port = process.env.PORT || 8080;
 // var port = process.env.OPENSHIFT_NODEJS_PORT || 8080
